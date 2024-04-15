70181800959204f4a8307d8fe98b397123c96733
Griffin-623 <79292572+Griffin-623@users.noreply.github.com>
2024-04-14 20:20:10 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,11 +7,23 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
+	
+		while(num>0){
+			digitList.add(num%10);
+			num = num/10;
+		}
 	    
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
+		for(int i = 0;i<digitList.length()-1;i++)
+		{
+		if(digitList.get(i)>=digitList.get(i+1)){
+			return false;
+		}
+		}
+		return true;
 		
 
 	}
